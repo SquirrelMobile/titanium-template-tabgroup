11d12693acc1f88d8a42502b5cb8f761ed451750
LEMAITRE Thomas <thomas@squirrel.fr>
2017-08-21 11:29:17 +0400

    init

---
+++ app/controllers/login/login.js
@@ -0,0 +1,105 @@
+/**
+ * @class Controller.login.login
+ * Display login view
+ *
+ */
+
+/**
+ * @method Controller
+ * Display login view
+ * @param  {Arguments} args Arguments passed to the controller
+ */
+(function constructor(args){
+
+  if(args.closeApp && OS_ANDROID){
+    $.win.addEventListener("androidback", function(){
+      var activity = Titanium.Android.currentActivity;
+      activity.finish();
+    });
+  }
+
+  $.login.listener('return', function(){
+    $.password.focus();
+  });
+
+  $.password.listener('return', function(){
+    connect();
+  });
+
+})($.args);
+
+
+/**
+ * openWin - open Signup or Lost password
+ *
+ * @param  {Object} e clicked object
+ */
+function openWin(e){
+
+  var type = e.source.type;
+  var controller = '';
+
+  switch (type) {
+    case 'signup':
+      controller = 'signup/signup';
+      break;
+    case 'lostPassword':
+      controller = 'login/lostPassword';
+    default:
+    break;
+
+  }
+
+  $.nav.openWindow(Alloy.createController(controller, e).getView());
+
+}
+
+/**
+ * connect - connection function
+ *
+ * @param  {object} e
+ */
+function connect(e){
+
+    var password = $.password.getValue();
+    var login = $.login.getValue();
+
+    if(!require('core').valideEmail(login)){
+      Ti.UI.createAlertDialog({
+        title : 'Attention',
+        message : 'Merci de saisir un email valide'
+      }).show();
+      return false;
+    }
+
+    if(password && login){
+
+      /*Alloy.Globals.loading.show(L("loading"));
+      //WS LOGIN
+      var obj = {
+        email : $.login.getValue(),
+        password : $.password.getValue()
+      };
+
+      Alloy.Globals.Api.signin({body:obj},function(e){
+
+      });*/
+      Ti.App.Properties.setBool('isConnected', true);
+      Alloy.createController('index').getView();
+
+    }
+
+}
+
+
+/**
+ * next - focus next field
+ *
+ * @param  {object} e field
+ */
+function next(e){
+
+  if($[e.source.next])
+    $[e.source.next].focus();
+
+}
---
+++ app/controllers/login/lostPassword.js
@@ -0,0 +1,65 @@
+/**
+ * @class Controller.lostPassword.lostPassword
+ * Display lostPassword view
+ *
+ */
+
+/**
+ * @method Controller
+ * Display lostPassword view
+ * @param  {Arguments} args Arguments passed to the controller
+ */
+(function constructor(args){
+
+  $.navbar.load({
+    back : {
+      visible : true
+    },
+    logo : {
+      visible : true
+    }
+  });
+
+})($.args);
+
+
+/**
+ * submit - submit function
+ *
+ * @param  {type} e description
+ */
+function submit(e){
+
+  var email = $.email.getValue();
+
+  if(!require('core').valideEmail(email)){
+    Ti.UI.createAlertDialog({
+      title : 'Attention',
+      message : 'Merci de saisir un email valide'
+    }).show();
+    return false;
+  }
+  
+  //Alloy.Globals.loading.show("Chargement...");
+  /*Alloy.Globals.Api.lostPassword({body :{email : $.email.value}}, function(e){
+
+  });*/
+  Ti.UI.createAlertDialog({
+    title : 'Confirmation',
+    message : 'Un email vient de vous être envoyé'
+  }).show();
+  close();
+
+
+}
+
+
+/**
+ * close - Close the window
+ *
+ * @param  {type} e description
+ * @return {type}   description
+ */
+function close(e){
+  $.win.close();
+}
